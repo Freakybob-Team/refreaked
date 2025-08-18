2ba761485e1faccc63deb8d73096422aea8ea04a
wish13yt <100239673+wish13yt@users.noreply.github.com>
2025-08-18 07:10:34 -0500

    greg

--- site/worker.js
+++ site/worker.js
@@ -3,14 +3,10 @@ export default {
     try {
       return await env.ASSETS.fetch(request);
     } catch (err) {
-      if (err.status === 404) {
         return new Response(
           await env.ASSETS.fetch(new Request("https://refreaked.wishinawel.workers.dev/404.html")),
           { status: 404, headers: { "Content-Type": "text/html" } }
-        );
+        )
       }
-
-      return new Response("Internal Server Error", { status: 500 });
-    }
   }
 };
