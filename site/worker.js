86a234f5ccc5e6729a24a9efad8c4792868c2181
wish13yt <100239673+wish13yt@users.noreply.github.com>
2025-08-18 07:14:04 -0500

    gtrg

--- site/worker.js
+++ site/worker.js
@@ -3,10 +3,20 @@ export default {
     try {
       return await env.ASSETS.fetch(request);
     } catch (err) {
-        return new Response(
-          await env.ASSETS.fetch(new Request("https://refreaked.wishinawel.workers.dev/404.html")),
-          { status: 404, headers: { "Content-Type": "text/html" } }
-        )
+      let notFoundPage;
+
+      try {
+        notFoundPage = await env.ASSETS.fetch(new Request("https://refreaked.wishinawel.workers.dev/404.html"));
+      } catch {
+        notFoundPage = new Response("<h1>404 - Page not found</h1><p>There was also an error loading the 404 page!</p>", {
+          headers: { "Content-Type": "text/html" }
+        });
       }
+
+      return new Response(await notFoundPage.text(), {
+        status: 404,
+        headers: { "Content-Type": "text/html" }
+      });
+    }
   }
 };
