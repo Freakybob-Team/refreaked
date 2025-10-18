18e2745fa8dffbe82a2135134e971975a0fcc58f
Wish <wishyt@proton.me>
2025-10-18 15:52:02 -0500

    team page

---
+++ site/js/team.js
@@ -0,0 +1,18 @@
+function wish() {
+    info.innerHTML = "Wish is a maintainer of Freakybob Team and also made the Freakybob Team organization. You can visit Wish's website at <a id='ignorefont' href='https://wish.freakybob.site'>https://wish.freakybob.site</a>!";
+}
+function squirrel() {
+    info.innerHTML = "Squirrel is a maintainer of Freakybob Team, the owner of freakybob.site, and is a back and front end developer. You can visit his website at <a id='ignorefont' href='https://squirrel.freakybob.site'>https://squirrel.freakybob.site</a>!";
+}
+function mpax235() {
+    info.innerHTML = "Mpax235 (aka mpax) is a developer at Freakybob Team, and learns a lot about different languages really fast. You can visit his website at <a id='ignorefont' href='https://mpax235.freakybob.site'>https://mpax235.freakybob.site</a>!";
+}
+function names() {
+    info.innerHTML = "Names is a developer at Freakybob Team, who is really stupid. You can visit his website at <a id='ignorefont' href='https://names.freakybob.site'>https://names.freakybob.site</a>!";
+}
+function noma() {
+    info.innerHTML = "Nomaakip (aka noma, hacks.guide) is a maintainer at Freakybob Team, who is a back and front end developer. You can visit her website at <a id='ignorefont' href='https://swag.freakybob.site'>https://swag.freakybob.site</a>!";
+}
+function closing() {
+    info.innerText = "";
+}
