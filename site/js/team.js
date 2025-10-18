a2ec5a3c0e9901e369b3b8613cc3293fff27a0ed
mpax235 <168873890+mpax235@users.noreply.github.com>
2025-10-18 14:46:43 -0700

    change code

--- site/js/team.js
+++ site/js/team.js
@@ -1,24 +1,16 @@
-function wish() {
-    info.innerHTML = "Wish is a maintainer of Freakybob Team and also made the Freakybob Team organization. You can visit Wish's website at <a id='ignorefont' href='https://wish.freakybob.site'>https://wish.freakybob.site</a>!";
-}
-function squirrel() {
-    info.innerHTML = "Squirrel is a maintainer of Freakybob Team, the owner of freakybob.site, and is a back and front end developer. You can visit his website at <a id='ignorefont' href='https://squirrel.freakybob.site'>https://squirrel.freakybob.site</a>!";
-}
-function mpax235() {
-    info.innerHTML = "Mpax235 (aka mpax) is a developer at Freakybob Team, and learns a lot about different languages really fast. You can visit his website at <a id='ignorefont' href='https://mpax235.freakybob.site'>https://mpax235.freakybob.site</a>!";
-}
-function names() {
-    info.innerHTML = "Names is a developer at Freakybob Team, who is really stupid. You can visit his website at <a id='ignorefont' href='https://names.freakybob.site'>https://names.freakybob.site</a>!";
-}
-function noma() {
-    info.innerHTML = "Nomaakip (aka noma, hacks.guide) is a maintainer at Freakybob Team, who is a back and front end developer. You can visit her website at <a id='ignorefont' href='https://swag.freakybob.site'>https://swag.freakybob.site</a>!";
-}
-function hyper() {
-    info.innerHTML = "Hypercuber (aka hyper) is an artist at Freakybob Team, who made the logo and uses Canva for their creations. You can visit their website at <a id='ignorefont' href='https://hyperstraw.straw.page/'>https://hyperstraw.straw.page/</a>!";
-}
-function twthtr() {
-    info.innerHTML = "#2 Twitter Hater (aka twitter hater, twtr hater) is an artist at Freakybob Team who made the original MaintenanceBob (who was the old mascot), and uses Krita for their creations. You can visit her SpaceHey at <a id='ignorefont' href='https://spacehey.com/nvtbvster'>https://spacehey.com/nvtbvster</a>!";
-}
-function closing() {
-    info.innerText = "";
+const info = document.getElementById('info');
+
+const memberDescriptions = [
+    "Squirrel is a maintainer of Freakybob Team, the owner of freakybob.site, and is a back and front end developer. You can visit his website at <a id='ignorefont' href='https://squirrel.freakybob.site'>https://squirrel.freakybob.site</a>!",
+    "Wish is a maintainer of Freakybob Team and also made the Freakybob Team organization. You can visit Wish's website at <a id='ignorefont' href='https://wish.freakybob.site'>https://wish.freakybob.site</a>!",
+    "Nomaakip (aka noma, hacks.guide) is a maintainer at Freakybob Team, who is a back and front end developer. You can visit her website at <a id='ignorefont' href='https://swag.freakybob.site'>https://swag.freakybob.site</a>!",
+    "Names is a developer at Freakybob Team, who is really stupid. You can visit his website at <a id='ignorefont' href='https://names.freakybob.site'>https://names.freakybob.site</a>!",
+    "#2 Twitter Hater (aka twitter hater, twtr hater) is an artist at Freakybob Team who made the original MaintenanceBob (who was the old mascot), and uses Krita for their creations. You can visit her SpaceHey at <a id='ignorefont' href='https://spacehey.com/nvtbvster'>https://spacehey.com/nvtbvster</a>!",
+    "Mpax235 (aka mpax) is a developer at Freakybob Team, and learns a lot about different languages really fast. You can visit his website at <a id='ignorefont' href='https://mpax235.freakybob.site'>https://mpax235.freakybob.site</a>!",
+    "Hypercuber (aka hyper) is an artist at Freakybob Team, who made the logo and uses Canva for their creations. You can visit their website at <a id='ignorefont' href='https://hyperstraw.straw.page/'>https://hyperstraw.straw.page/</a>!",
+    ""
+];
+
+function showMember(memberID) {
+    info.innerHTML = memberDescriptions[memberID];
 }
